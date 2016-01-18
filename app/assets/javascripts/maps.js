7f3b1c6fa61799d41a5bacbc0aab48bc5a753f97
La Baik <labaik237@gmail.com>
2016-01-18 13:20:48 -0800

    adding bike layer

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -1,8 +1,8 @@
 function initialize() {
   var mapProperties = {
-    center: new google.maps.LatLng(-34.397, 150.644),
-    zoom: 17,
-    mapTypeId: google.maps.MapTypeId.TERRAIN
+    center: new google.maps.LatLng(34.079156, -118.251276),
+    zoom: 14,
+    mapTypeId: google.maps.MapTypeId.BICYCLING
   };
   var map = new google.maps.Map(document.getElementById("googleMap"), mapProperties);
 
@@ -11,6 +11,8 @@ function initialize() {
   });
   marker.setMap(map);
 
+  var bikeLayer = new google.maps.BicyclingLayer();
+  bikeLayer.setMap(map);
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
